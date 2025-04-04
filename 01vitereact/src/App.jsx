95f028b72b4c3e0dd45ab1cfea7d7f4c3decb6e4
Aditya Kumar <adityakumar9074@gmail.com>
2025-04-04 13:46:16 +0530

    second project
    vite-react project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Chai from "./chai"
+
+function App() {
+  const username = "chai aur code"
+
+  return (
+   //  <> </> this called fragement in react
+   <>  
+   
+   <Chai/>
+   <h1>Chai aur react</h1>
+   <p>test para {username}</p>
+   </>
+  )
+}
+
+export default App
